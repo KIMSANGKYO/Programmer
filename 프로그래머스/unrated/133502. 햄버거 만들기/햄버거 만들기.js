29ee30f65d329f67cd8d3dc61a7a71c12148e8d8
KIMSANGKYO <115609325+KIMSANGKYO@users.noreply.github.com>
2023-05-09 21:53:35 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
@@ -1,21 +1,31 @@
 function solution(ingredient) {
-   // 1 빵 2 야채 3 고기 
-    // 1231로 만든다 . 
-    // 한번 만들면 남은 배열구조로도 만들수있다 
+//    // 1 빵 2 야채 3 고기 
+//     // 1231로 만든다 . 
+//     // 한번 만들면 남은 배열구조로도 만들수있다 
     
-    // 만들수있는 햄버거 카운트 
-    let result = 0 ; 
-    for(let i=0; i<ingredient.length; i++){
-        // 4개씩 자르고 문자열상태로 비교 
-        if(ingredient.slice(i,i+4).join('')==='1231'){  
-            // 같을때마다 카운트 추가 
-            result++
-            // 해당부분 배열에서 삭제시키기 
-            ingredient.splice(i,4);
-            // i 값을 리셋 시켜준다 . 
-            // i 는 반복문으로 증가하므로 증가하는 1값 제외 3 을 빼서 리셋 
-            i=i-3;
-        }
+//     // 만들수있는 햄버거 카운트 
+//     let result = 0 ; 
+//     for(let i=0; i<ingredient.length; i++){
+//         // 4개씩 자르고 문자열상태로 비교 
+//         if(ingredient.slice(i,i+4).join('')==='1231'){  
+//             // 같을때마다 카운트 추가 
+//             result++
+//             // 해당부분 배열에서 삭제시키기 
+//             ingredient.splice(i,4);
+//             // i 값을 리셋 시켜준다 . 
+//             // i 는 반복문으로 증가하므로 증가하는 1값 제외 3 을 빼서 리셋 
+//             i=i-3;
+//         }
+//     }
+//     return result;
+    
+    let hamburger =[];
+    let count = 0; 
+    for (let i=0; i<ingredient.length; i++){
+        hamburger.push(ingredient[i]);
+        if(hamburger[hamburger.length-4]===1&&hamburger[hamburger.length-3]===2&&hamburger[hamburger.length-2]===3&&hamburger[hamburger.length-1]===1)
+            {count ++ ; hamburger.splice(-4)}
+        
     }
-    return result;
+    return count;
 }
