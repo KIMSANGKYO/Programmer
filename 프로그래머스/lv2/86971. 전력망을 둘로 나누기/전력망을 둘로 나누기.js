5cb5d732555ca80c305f3db9748d8519fb5e3a44
KIMSANGKYO <115609325+KIMSANGKYO@users.noreply.github.com>
2023-07-18 19:40:51 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.19 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -0,0 +1,53 @@
+function solution(n, wires) {
+    const graph = createGraph(n, wires);
+    let minDiff = Infinity;
+
+    for (let i = 0; i < wires.length; i++) {
+        const [v1, v2] = wires[i];
+        const count1 = countNodes(graph, v1, v2); 
+        const count2 = n - count1; 
+        const diff = Math.abs(count1 - count2); 
+        minDiff = Math.min(minDiff, diff); 
+    }
+
+    return minDiff;
+}
+
+
+function createGraph(n, wires) {
+    const graph = Array.from({ length: n + 1 }, () => []);
+
+    for (let i = 0; i < wires.length; i++) {
+        const [v1, v2] = wires[i];
+        graph[v1].push(v2);
+        graph[v2].push(v1);
+    }
+
+    return graph;
+}
+
+
+function countNodes(graph, start, ban) {
+    let count = 1;
+    const stack = [start];
+    const visited = new Set();
+    visited.add(start);
+
+    while (stack.length > 0) {
+        const node = stack.pop();
+
+        for (let i = 0; i < graph[node].length; i++) {
+            const next = graph[node][i];
+
+            if (next === ban || visited.has(next)) {
+                continue;
+            }
+
+            stack.push(next);
+            visited.add(next);
+            count++;
+        }
+    }
+
+    return count;
+}
