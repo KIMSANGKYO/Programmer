6e1b24ef6853b6b5655acb662c8d31277d0b756e
KIMSANGKYO <115609325+KIMSANGKYO@users.noreply.github.com>
2023-08-06 21:07:18 +0900

    [unrated] Title: 추억 점수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
@@ -1,4 +1,16 @@
 function solution(name, yearning, photo) {
-    var answer = [];
-    return answer;
+    const scores = [];
+  
+    for (const pic of photo) {
+        let totalScore = 0;
+        for (const person of pic) {
+            const index = name.indexOf(person);
+            if (index !== -1) {
+                totalScore += yearning[index];
+            }
+        }
+        scores.push(totalScore);
+    }
+
+    return scores;
 }
