7a078b3a1b8463b5db1cba0b1e36aee28f630a55
KIMSANGKYO <115609325+KIMSANGKYO@users.noreply.github.com>
2023-12-09 22:45:59 +0900

    [level 2] Title: 전화번호 목록, Time: 156.78 ms, Memory: 96.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,23 +1,15 @@
 function solution(phone_book) {
-    const hashSet = new Set();
+    let answer = true;
 
-    // 해시셋에 전화번호를 추가합니다.
-    for (let i = 0; i < phone_book.length; i++) {
-        hashSet.add(phone_book[i]);
-    }
+    const table = new Set(phone_book);
 
-    // 각 전화번호의 가능한 모든 접두어에 대해 해시셋을 검사합니다.
-    for (let i = 0; i < phone_book.length; i++) {
-        let prefix = "";
-        for (let j = 0; j < phone_book[i].length; j++) {
-            prefix += phone_book[i][j];
-            if (hashSet.has(prefix) && prefix !== phone_book[i]) {
-                // 현재 전화번호의 접두어가 해시셋에 존재하고 현재 전화번호와 다를 경우 false를 반환합니다.
+    for(const phone_number of phone_book) {
+        for(let i = 1; i < phone_number.length; i++) {
+            if(table.has(phone_number.slice(0, i))) {
                 return false;
             }
         }
     }
 
-    // 모든 전화번호에 대해 접두어 여부를 확인했을 때, true를 반환합니다.
-    return true;
+    return answer;
 }
